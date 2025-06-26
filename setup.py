1eb72e0c5594cb7bc20c13845377ec74928e5791
Sarthak Das <sdas4@icloud.com>
2025-06-26 10:40:07 +0100

    Update dependencies in setup

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 
 from setuptools import setup, find_packages
 
-# TODO: impliment a fix on this
+
 setup(
     name="franka",
     version="0.1.0",
-    description="A brief description of your package",
+    description="Franka Panda robot driver for the ARK framework",
     author="Your Name",
     author_email="your.email@example.com",
     packages=find_packages(),
     install_requires=[
-        "some_dependency>=1.0.0",
+        "ark",
+        "franky",
+        "numpy",
+        "scipy",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
