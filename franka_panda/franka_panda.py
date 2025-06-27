439f8c8c47e3b6bf20e65aa3c79c30eb2316ef01
sarthakdas <sdas4@icloud.com>
2025-06-27 14:35:17 +0100

    temp fix jiacheng

--- franka_panda/franka_panda.py
+++ franka_panda/franka_panda.py
@@ -11,7 +11,7 @@ from ark.system.driver.robot_driver import RobotDriver
 from ark.system.pybullet.pybullet_robot_driver import BulletRobotDriver
 from ark.tools.log import log
 from arktypes import flag_t, joint_group_command_t, joint_state_t
-
+from arktypes.utils import unpack, pack
 # from franka_driver import FrankaResearch3Driver
 
 
@@ -38,15 +38,15 @@ class FrankaPanda(Robot):
         #######################################
         self._joint_cmd_msg, self._cartesian_cmd_msg =  None, None
         
-        self.joint_group_command = self.name + "/joint_group_command"
-        self.cartesian_position_control_name = self.name + "/cartesian_command"
+        self.joint_group_command_ch = self.name + "/joint_group_command"
+        self.cartesian_position_control_ch = self.name + "/cartesian_command"
 
         if self.sim:
-            self.subscriber_name = self.joint_group_command + "/sim"
-            self.cartesian_position_control_name = self.cartesian_position_control_name + "/sim"
+            self.joint_group_command_ch = self.joint_group_command_ch + "/sim"
+            self.cartesian_position_control_ch = self.cartesian_position_control_ch + "/sim"
 
-        self.create_subscriber(self.joint_group_command, joint_group_command_t, self._joint_group_command_callback)
-        self.create_subscriber(self.cartesian_position_control_name, joint_group_command_t, self._cartesian_position_command_callback)
+        self.create_subscriber(self.joint_group_command_ch, joint_group_command_t, self._joint_group_command_callback)
+        self.create_subscriber(self.cartesian_position_control_ch, joint_group_command_t, self._cartesian_position_command_callback)
 
         if self.sim == True: 
             self.publisher_name = self.name + "/joint_states/sim"
@@ -59,38 +59,37 @@ class FrankaPanda(Robot):
                 (self.publisher_name, joint_state_t)
             ])
 
-    def get_robot_data(self):
+        self.joint_group_command = None
+        self.cartesian_position_control_command = None
+
+    def control_robot(self):
         '''
         will be called at the fequency dictated in the config
         handles the control of the robot and the 
         '''
-        if self._joint_cmd_msg:
-            msg = self._joint_cmd_msg
-            group_name = msg.name
-            cmd = msg.cmd
+        if self.joint_group_command:
             cmd_dict = {}
-            for joint, goal in zip(list(self.joint_groups[group_name]["actuated_joints"]), cmd):
+            print(f"Received joint group command: {self.joint_group_command}")
+            group_name = self.joint_group_command['name']
+            for joint, goal in zip(list(self.joint_groups[self.joint_group_command['name']]["actuated_joints"]), self.joint_group_command['cmd']):
                 cmd_dict[joint] = goal
             self._joint_cmd_msg = None
-            self.control_joint_group(group_name, cmd_dict)
-        elif self._cartesian_cmd_msg:
-            msg = self._cartesian_cmd_msg
-            group_name = msg.name
-            cmd = msg.cmd
-            cmd_dict = {}
-            for joint, goal in zip(list(self.joint_groups[group_name]["actuated_joints"]), cmd):
-                cmd_dict[joint] = goal
-            self._cartesian_cmd_msg = None
-            self.control_cartesian(group_name, cmd_dict)
-            
-        # print(self.get_joint_positions())
+            control_mode = self.joint_groups[group_name]["control_mode"]
+            print(f"Controlling {group_name} with mode {control_mode} and command {cmd_dict}")
+            self.control_joint_group(control_mode, cmd_dict)
+
+    def get_state(self) -> Dict[str, Any]:
+        """
+        Returns the current state of the robot.
+        This method is called by the base class to get the state of the robot.
+        """
         return self.get_joint_positions()
 
-    def pack_data(self, pos_dict):
+    def pack_data(self, state: Dict[str, Any]) -> Dict[str, Any]:
         msg = joint_state_t()
-        msg.n = len(pos_dict)
-        msg.name = list(pos_dict.keys())
-        msg.position = list(pos_dict.values())
+        msg.n = len(state)
+        msg.name = list(state.keys())
+        msg.position = list(state.values())
         msg.velocity = [0.0] * msg.n
         msg.effort = [0.0] * msg.n
 
@@ -102,22 +101,26 @@ class FrankaPanda(Robot):
     ##      Franka Subscriber Callbacks               ##
     ####################################################
     def _joint_group_command_callback(self, t, channel_name, msg):
-        self._joint_cmd_msg = msg
+        cmd, name = unpack.unpack_joint_group_command(msg)
+        self.joint_group_command = {
+            "cmd": cmd,
+            "name": name,
+        }
 
     def _cartesian_position_command_callback(self, t, channel_name, msg): 
-        self._cartesian_cmd_msg = msg
+        self.cartesian_position_control_name = unpack.unpack_joint_group_command_t(msg)
 
     ####################################################
     ##       Franka Custom Control Methods            ##
     ##    note: control_joint_group is default        ##
     ####################################################
 
-    @robot_control
-    def control_cartesian(self, control_mode: str, joints: List[str], cmd: Dict[str, float], **kwargs) -> None: 
-        if self.sim == True: 
-            log.error("Cartesian Positon control is not availble for Franka in Pybullet yet") #TODO add Cartesian Position Control
-        elif self.sim == False: 
-            self._driver.pass_cartesian_control_cmd(control_mode, joints, cmd, **kwargs)
+    # @robot_control
+    # def control_cartesian(self, control_mode: str, joints: List[str], cmd: Dict[str, float], **kwargs) -> None: 
+    #     if self.sim == True: 
+    #         log.error("Cartesian Positon control is not availble for Franka in Pybullet yet") #TODO add Cartesian Position Control
+    #     elif self.sim == False: 
+    #         self._driver.pass_cartesian_control_cmd(control_mode, joints, cmd, **kwargs)
 
     #####################################################
 
